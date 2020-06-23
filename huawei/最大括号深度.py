b9d51b8874510cefdc3582f19ba925ee96ce934a
LeGr4ndK <37144515+Akazfu@users.noreply.github.com>
2020-06-23 11:59:34 +0800

    g

--- huawei/最大括号深度.py
+++ huawei/最大括号深度.py
@@ -1,7 +1,7 @@
 input_str = '[][][][][]{}{{{{}}}}((([[[]]])))'
-left = {'(', '[', '{'}
-right = {')', ']', '}'}
-check = []
+left = ['(', '[', '{']
+right = [')', ']', '}']
+stack = []
 count, deep = 0, 0
 
 if (input_str[0] not in left) or (input_str[-1] not in right):
@@ -9,33 +9,14 @@ if (input_str[0] not in left) or (input_str[-1] not in right):
 else:
     for cha in input_str:
         if cha in left:
-            check.append(cha)
+            stack.append(cha)
             count += 1
+
         if cha in right:
-            if cha == ')':
-                if check.pop() == '(':
-                    if count > deep:
-                        deep = count
-                    count = 0
-                else:
-                    deep = 0
-                    break
-            elif cha == ']':
-                if check.pop() == '[':
-                    if count > deep:
-                        deep = count
-                    count = 0
-                else:
-                    deep = 0
-                    break
-            elif cha == '}':
-                if check.pop() == '{':
-                    if count > deep:
-                        deep = count
-                    count = 0
-                else:
-                    deep = 0
-                    break
+            if right.index(cha) == left.index(stack.pop()):
+                if count > deep:
+                    deep = count
+                count = 0
             else:
                 deep = 0
                 break
